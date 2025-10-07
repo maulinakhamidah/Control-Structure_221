1af49a0b4016d01c91f8cc8f6e87a01e0a15720d
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-10-07 10:47:45 +0700

    prak1

---
+++ prak1.py
@@ -0,0 +1,14 @@
+percentage = float(input("Masukkan peresentase nilai sisw:"))
+
+if percentage >= 90:
+    print("Excellent performance")
+elif percentage >= 80:
+    print("Very good performance")
+elif percentage >= 70:
+    print("good performance")
+elif percentage >= 60:
+    print("Avarage performance")
+else:
+    print("Needs improvement")
+
+    
